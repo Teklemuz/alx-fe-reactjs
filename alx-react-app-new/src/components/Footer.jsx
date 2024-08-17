cc1d1bb125db8826cea75647c57c8223f2f3a64d
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-17 15:08:16 +0300

    Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return (
+      <footer
+        style={{
+          backgroundColor: '#404040',
+          color: '#fff',               
+          textAlign: 'center',        
+          padding: '15px',          
+          margin: '15px',            
+          borderRadius: '8px',        
+          fontSize: '14px',            
+          borderTop: '1px solid #555'  
+        }}
+      >
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
