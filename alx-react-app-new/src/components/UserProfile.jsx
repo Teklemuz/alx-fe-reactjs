b4eac4d49e5177a4872b977d0b6457485eaeac1d
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-17 15:09:07 +0300

    UserProfile.jsx

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,41 @@
+const UserProfile = (props) => {
+    return (
+      <div
+        style={{
+          border: '1px solid gray', 
+          padding: '15px',      
+          margin: '15px',            
+        }}
+      >
+        <h2
+          style={{
+            color: 'navy',       
+            fontSize: '18px',       
+            margin: '0 0 10px 0'    
+          }}
+        >
+          {props.name}
+        </h2>
+        <p
+          style={{
+            fontSize: '14px',      
+            margin: '0 0 5px 0'     
+          }}
+        >
+          Age: <span style={{ fontWeight: 'bold', color: 'darkgray' }}>{props.age}</span>
+        </p>
+        <p
+          style={{
+            fontSize: '14px',       
+            color: 'darkslategray',
+            margin: '0'          
+          }}
+        >
+          Bio: {props.bio}
+        </p>
+      </div>
+    );
+  };
+  
+  export default UserProfile;
+  
