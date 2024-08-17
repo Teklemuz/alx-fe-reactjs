612b94cee7c5ef31086c33d5353775d8085ad2e9
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-17 15:07:46 +0300

    MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,20 @@
+function MainContent() {
+    return (
+      <main
+        style={{
+          backgroundColor: '#d3d3d3', 
+          padding: '15px',             
+          margin: '15px',            
+          borderRadius: '10px',         
+          boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+          fontSize: '16px',            
+          color: '#333'                
+        }}
+      >
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  
