f186c23919f527390a257e7e01e587799502c94f
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-24 08:18:55 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import useRecipeStore from '../recipeStore';
+import React, { useEffect } from 'react';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const { filteredRecipes, recipes } = useRecipeStore(state => ({
+    filteredRecipes: state.filteredRecipes,
+    recipes: state.recipes
+  }));
+
+  const filterRecipes = useRecipeStore(state => state.setSearchTerm);
+
+  useEffect(() => {
+    filterRecipes(''); 
+  }, [recipes]);
 
   return (
     <div>
-      {recipes.length === 0 ? (
-        <p>No recipes available</p>
-      ) : (
-        recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      )}
+      {filteredRecipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
