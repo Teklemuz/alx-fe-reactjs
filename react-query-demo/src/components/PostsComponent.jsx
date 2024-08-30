b83657278e7e7b8c08f75f5538bafcea57d8bd73
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-30 16:53:44 +0300

    Create PostsComponent.jsx

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+function PostsComponent() {
+  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 5 * 60 * 1000, // 5 minutes
+    cacheTime: 10 * 60 * 1000, // 10 minutes
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
