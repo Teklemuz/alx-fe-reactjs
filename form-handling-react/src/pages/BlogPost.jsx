722dca28c9b48b7623131368a57695d4fd664deb
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-31 15:01:40 +0300

    Delete pages/BlogPost.jsx

--- form-handling-react/src/pages/BlogPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-function BlogPost() {
-  const { postId } = useParams();
-  return <div>Blog Post {postId}</div>;
-}
-
-export default BlogPost;
