165979acc876decfdc4eb405d8e9954a093cc503
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-08-31 15:58:27 +0300

    Update App.test.js

--- react-todo/src/__tests__/App.test.js
+++ react-todo/src/__tests__/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from '../App'; // Import the App component
+import App from '../App';
 
 describe('App Component', () => {
   test('renders the TodoList component', () => {
     render(<App />);
-    expect(screen.getByText('Add a new todo')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Add a new todo')).toBeInTheDocument();
+    expect(screen.getByText('Add Todo')).toBeInTheDocument(); 
   });
 });
