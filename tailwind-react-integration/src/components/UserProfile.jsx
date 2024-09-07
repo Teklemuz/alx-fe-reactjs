eb2aa015b2dc45800185871458b63f2b057a17f5
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-09-07 15:53:02 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-12 md:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-12 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
+        John Doe
+      </h1>
       <p className="text-sm sm:text-base md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
