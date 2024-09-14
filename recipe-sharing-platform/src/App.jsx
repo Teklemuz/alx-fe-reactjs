855e2331b89d74c99c1123c8f764dc7bcc5d1114
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-09-14 14:15:18 +0300

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import AddRecipePage from './pages/AddRecipePage';
 import RecipeDetail from './components/RecipeDetail';
 import './App.css';
 
