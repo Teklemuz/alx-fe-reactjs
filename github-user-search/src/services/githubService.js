4832ac9e5f700ce2f798ba54ed5222a8deb8df48
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-09-20 14:16:59 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users';
 
-export const fetchUser = async (username) => {
-  const response = await axios.get(`${GITHUB_API_URL}/${username}`);
-  return response.data;
+export const searchUsers = async (username, location = '', minRepos = 0) => {
+  const params = {
+    q: `${username}${location ? `+location:${location}` : ''}`,
+  };
+
+  const response = await axios.get(GITHUB_SEARCH_API_URL, { params });
+  return response.data.items; // Return the array of user objects
 };
